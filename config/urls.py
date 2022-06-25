cf4367b19074cf1b264b18ea6bd88ecc68b3cbd5
daurenotarbai.00 <daurenotarbai.00@gmail.com>
2022-06-26 00:29:39 +0600

    fix

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
+# import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("apps.urls")),
-    path("__debug__/", include(debug_toolbar.urls)),
+    # path("__debug__/", include(debug_toolbar.urls)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
