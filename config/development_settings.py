bd942ba06c1d7e5692eb68709001463e44db69ef
daurenotarbai <daurenotarbai.00@gmail.com>
2022-07-11 21:11:11 +0600

    added fix in dev SETTINGS

--- config/development_settings.py
+++ config/development_settings.py
@@ -1,4 +1,9 @@
+from .base_settings import *
+
 DEBUG = False
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/static/'
 STATIC_ROOT = '/vol/web/static'
