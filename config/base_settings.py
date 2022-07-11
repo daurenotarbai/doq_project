aaa64c83595659a1d2226046675408c695bb0820
daurenotarbai <daurenotarbai.00@gmail.com>
2022-07-11 10:05:40 +0600

    added fix in settings

--- config/base_settings.py
+++ config/base_settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/static/'
-STATIC_ROOT = '/vol/web/static'
-MEDIA_URL = '/static/media/'
-MEDIA_ROOT = '/vol/web/media'
+STATIC_URL = os.getenv("STATIC_URL", '/static/')
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+MEDIA_URL = os.getenv("MEDIA_URL", '/media/')
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
