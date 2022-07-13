3ca97d6ded254ff544d6f6b72633cf1884fa5416
daurenotarbai <daurenotarbai.00@gmail.com>
2022-07-13 10:50:28 +0600

    add fix

--- config/development_settings.py
+++ config/development_settings.py
@@ -1,7 +1,7 @@
 from .base_settings import *
 
 DEBUG = False
-
+INTERNAL_IPS.append('46.101.243.200')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
