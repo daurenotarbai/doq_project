aedd45c64c7792e37596f1fbc2970d46a48981c3
daurenotarbai <daurenotarbai.00@gmail.com>
2022-07-14 21:04:25 +0600

    fix in docker

--- config/development_settings.py
+++ config/development_settings.py
@@ -2,7 +2,7 @@ from .base_settings import *
 
 DEBUG = False
 INTERNAL_IPS = [
-    '127.0.0.1', '46.101.243.200',
+    '46.101.243.200',
 ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
